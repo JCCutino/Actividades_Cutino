8b767e549946bca76199c0fc290779857b8b54c0
Juan Cutino <jcutinocortacero@alumos.ilernasevilla.com>
2023-09-28 11:11:47 +0200

    Enunciado 6

---
+++ Servidor/ACTIVIDAD 2/006etapa.php
@@ -0,0 +1,14 @@
+<?php
+/*A partir de una edad muestra por pantalla:
+bebé si tiene menos de 3 años
+niño si tiene entre 3 y 12 años
+adolescente entre 13 y 17 años
+adulto entre 18 y 66
+jubilado a partir de 67
+ */
+
+ $edad = 30;
+
+
+
+?>

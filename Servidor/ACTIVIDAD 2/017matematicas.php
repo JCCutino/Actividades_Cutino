1089223d94e1e234c40da2d9d6716472daacca35
Juan Cutino <jcutinocortacero@alumos.ilernasevilla.com>
2023-09-29 13:49:40 +0200

    Comentario actividad 17

---
+++ Servidor/ACTIVIDAD 2/017matematicas.php
@@ -0,0 +1,11 @@
+<?php
+/* Añade las siguientes funciones:
+digitos(int $num): int ‚ devuelve la cantidad de dígitos de un número.
+digitoN(int $num, int $pos): int‚ devuelve el digito que ocupa, empezando por la izquierda, la posición $pos.
+quitaPorDetras(int $num, int $cant): int‚ le quita por detrás (derecha) $cant digitos.
+quitaPorDelante(int $num, int $cant): int‚ le quita por delante (izquierda) $cant digitos.
+Para probar las funciones, haz uso tanto de paso de argumentos posicionales como argumentos con nombre.
+*/
+
+
+?>

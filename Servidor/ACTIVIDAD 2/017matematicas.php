5563b83707e99c1f01b0dacf1d5518d722d10b68
JCCutino <jcutinocortacero@alumnos.ilernasevilla.com>
2023-10-01 15:20:59 +0200

    Actividad 17

--- Servidor/ACTIVIDAD 2/017matematicas.php
+++ Servidor/ACTIVIDAD 2/017matematicas.php
@@ -7,5 +7,41 @@ quitaPorDelante(int $num, int $cant): int‚ le quita por delante (izquierda) $c
 Para probar las funciones, haz uso tanto de paso de argumentos posicionales como argumentos con nombre.
 */
 
+function digitos(int $num): int {
+    return strlen((string)$num);
+}
 
+function digitoN(int $num, int $pos): int {
+    $numString = (string)$num;
+    
+    if ($pos >= 1 && $pos <= strlen($numString)) {
+        return (int)$numString[$pos - 1];
+    } else {
+        return -1; 
+    }
+}
+
+
+function quitaPorDetras(int $num, int $cant): int {
+    $numString = (string)$num;
+    return (int)substr($numString, 0, -$cant);
+}
+
+
+function quitaPorDelante(int $num, int $cant): int {
+    $numString = (string)$num;
+    return (int)substr($numString, $cant);
+}
+
+// Ejemplos de uso de las funciones
+$num = 123456789;
+
+echo "Cantidad de dígitos en $num: " . digitos($num) . "<br>";
+echo "Dígito en la posición 3 de $num: " . digitoN($num, 3) . "<br>";
+
+$numSinUltimosDosDigitos = quitaPorDetras($num, 2);
+echo "Quitar los últimos dos dígitos de $num: $numSinUltimosDosDigitos <br>";
+
+$numSinPrimerTresDigitos = quitaPorDelante($num, 3);
+echo "Quitar los primeros tres dígitos de $num: $numSinPrimerTresDigitos <br>";
 ?>

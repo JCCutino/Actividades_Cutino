c25c7e3f18f5c8cdc8aa2b9dbf8dc3fe73c6390e
JCCutino <jcutinocortacero@alumnos.ilernasevilla.com>
2023-10-01 19:33:24 +0200

    Actividad 8

---
+++ Servidor/ACTIVIDAD 2/008investiga.php
@@ -0,0 +1,18 @@
+<?php
+/*8.	008Investiga para qué sirve la instrucción match(), disponible desde PHP8 (https://www.php.net/manual/es/control-structures.match.php).
+Explica con un par de l√≠neas su propósito y mediante código demuestra su uso. */
+
+echo "La instrucción match en PHP 8 es una forma más poderosa y expresiva de hacer comparaciones. Funciona como un switch que te permite comparar una variable con varios valoresy ejecutar código según el patrón coincidente.";
+ 
+$valor = 3;
+echo "<br>";
+$resultado = match ($valor) {
+    1 => "Uno",
+    2, 3, 4 => "Dos, Tres o Cuatro",
+    5 => "Cinco",
+    default => "Otro Valor",
+};
+
+echo "Resultado: $resultado";
+ 
+?>

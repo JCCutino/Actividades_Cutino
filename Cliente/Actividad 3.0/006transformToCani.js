24c2a97268401688e0a0becccf0d56ea132c7403
JCCutino <jcutinocortacero@alumnos.ilernasevilla.com>
2023-10-04 20:58:50 +0200

    Actividad 6

--- Cliente/Actividad 3.0/006transformToCani.js
+++ Cliente/Actividad 3.0/006transformToCani.js
@@ -4,3 +4,35 @@
  Por ejemplo, si le pasamos a la función la cadena "una cadena cani es como ésta" 
  obtendremos "UnA KaDeNa kAnI Es kOmO EsTaHHH". Para ello, hay que alternar el uso
   de MAYÚSCULAS y minúsculas, sustituir la letra C por la K y añadir tres letras H al final.*/
+
+  function transformarACani(cadena) {
+    let resultado = "";
+    let mayusculas = true; 
+
+    for (let i = 0; i < cadena.length; i++) {
+        const letra = cadena[i];
+
+        if (letra.toLowerCase() === 'c') {
+            resultado += 'K';
+        } else {
+            resultado += mayusculas ? letra.toUpperCase() : letra.toLowerCase();
+        }
+
+        mayusculas = !mayusculas;
+    }
+
+
+    resultado += 'HHH';
+
+    return resultado;
+}
+
+let cadenaOriginal = "la cadena le brillo en lo oscuro";
+let cadenaTransformada = transformarACani(cadenaOriginal);
+alert(cadenaTransformada);
+cadenaOriginal = "Huele a Baccarat, fumándose un puro";
+cadenaTransformada = transformarACani(cadenaOriginal);
+alert(cadenaTransformada);
+cadenaOriginal = "Tiene cara que en la cama te da duro";
+cadenaTransformada = transformarACani(cadenaOriginal);
+alert(cadenaTransformada);

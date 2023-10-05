b7687e25f92f41c82a7f85290584f0f14cfd6450
Juan Cutino <jcutinocortacero@alumos.ilernasevilla.com>
2023-10-05 10:27:24 +0200

    Actividad 7

--- Cliente/Actividad 3.0/007rewriteWithArrowFunction.js
+++ Cliente/Actividad 3.0/007rewriteWithArrowFunction.js
@@ -1,6 +1,6 @@
 "use strict";
 /*Reemplace las expresiones de función con funciones de flecha en el código a continuación: */
-
+/*
 function ask(question, yes, no) {
 
     if (confirm(question)) yes();
@@ -17,4 +17,15 @@ function ask(question, yes, no) {
   
     function() { alert("You canceled the execution."); }
   
-  );
+  );*/
+  
+  let ask = (question, yes, no) => {
+    if (confirm(question)) yes();
+    else no();
+};
+
+ask(
+    "Do you agree?",
+    () => { alert("You agreed."); },
+    () => { alert("You canceled the execution."); }
+);

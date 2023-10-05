a52cce65634721e702013475f35fa27ba57182b0
Juan Cutino <jcutinocortacero@alumos.ilernasevilla.com>
2023-10-05 12:10:26 +0200

    Actividad 12

--- Cliente/Actividad 3.0/012createuserAccount.js
+++ Cliente/Actividad 3.0/012createuserAccount.js
@@ -3,3 +3,22 @@
 de usuario y una dirección de correo electrónico como argumentos obligatorios. También
 puede tomar un número de teléfono opcional. Si el número de teléfono no se proporciona,
 la cuenta se creará sin él. */
+function crearCuentaUsuario(nombreUsuario, correoElectronico, numeroTelefono) {
+
+    let cuentaUsuario = [nombreUsuario,correoElectronico];
+
+ 
+    if (numeroTelefono !== undefined) {
+        cuentaUsuario=cuentaUsuario.concat(numeroTelefono);
+    }
+
+    return cuentaUsuario;
+}
+
+
+let cuenta1 = crearCuentaUsuario("Carlos", "karlos@gmail.com");
+alert(cuenta1);
+
+
+let cuenta2 = crearCuentaUsuario("Jose", "pepe@gmail.com", "653727193");
+alert(cuenta2);

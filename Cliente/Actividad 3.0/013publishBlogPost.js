f21d30adf7b85c16b175ff465639627de3e4eb84
JCCutino <jcutinocortacero@alumnos.ilernasevilla.com>
2023-10-05 20:21:21 +0200

    Actividad 13

--- Cliente/Actividad 3.0/013publishBlogPost.js
+++ Cliente/Actividad 3.0/013publishBlogPost.js
@@ -3,3 +3,14 @@
 un título y un contenido como argumentos obligatorios. También puede tomar una lista
 de etiquetas opcional. Si no se proporcionan etiquetas, la publicación se creará sin ellas.
 */
+
+const crearPublicacion = (titulo, contenido, ...etiquetas) => {
+    return [titulo, contenido, ...etiquetas];
+};
+
+// Ejemplo de uso
+const primeraPublicacion = crearPublicacion("Mi primera publicacion", "Este es el contenido de la publicacion", ["JS", "HTML"]);
+alert(primeraPublicacion);
+
+const segundaPublicacion = crearPublicacion("Mi segunda publicacion", "Llueven botellas mientras suena una de Rauw");
+alert(segundaPublicacion);

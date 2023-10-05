3b13885535e83f2e88a3d099d6eec66a7852c80d
Juan Cutino <jcutinocortacero@alumos.ilernasevilla.com>
2023-10-05 12:35:55 +0200

    Actividad 15

--- Cliente/Actividad 3.0/015findMaxValue.js
+++ Cliente/Actividad 3.0/015findMaxValue.js
@@ -2,3 +2,12 @@
 /*Crea una función llamada findMaxValue que tome un array de números y utilice
  el método reduce para devolver el valor más grande. Utilice las funciones flecha y no haga uso de if else.
  */
+
+ let reduceArray = (array) => {
+    return array.reduce((max, valor) => Math.max(max,valor));
+  };
+  
+ 
+  let numeros = [5, 9, 3, 4, 7];
+  let resultado = reduceArray(numeros);
+  alert(resultado); 

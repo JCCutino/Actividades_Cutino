3ac318cf4c768e6fc848a73aa5a27fbad026d1d9
Juan Cutino <jcutinocortacero@alumos.ilernasevilla.com>
2023-10-05 12:28:12 +0200

    Actividad 14

--- Cliente/Actividad 3.0/014reduceArray.js
+++ Cliente/Actividad 3.0/014reduceArray.js
@@ -2,3 +2,12 @@
 /*Crea una función llamada reduceArray que tome un array de números y utilice
  el método reduce para devolver la suma de todos los elementos. Utilice las funciones flecha.
 */
+
+let reduceArray = (array) => {
+    return array.reduce((suma, valor) => suma + valor, 0);
+  };
+  
+ 
+  let numeros = [5, 2, 3, 4, 5];
+  let resultado = reduceArray(numeros);
+  alert(resultado); 

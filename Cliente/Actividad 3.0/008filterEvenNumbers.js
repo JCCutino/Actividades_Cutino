241a7319cc4981c01d9e3078de6f8a2ef49086b4
Juan Cutino <jcutinocortacero@alumos.ilernasevilla.com>
2023-10-05 11:02:15 +0200

    Actividad 8, 9

--- Cliente/Actividad 3.0/008filterEvenNumbers.js
+++ Cliente/Actividad 3.0/008filterEvenNumbers.js
@@ -4,3 +4,13 @@
 Escribe una función que tome un array de números y devuelva un nuevo array
 que solo contenga los números pares. Utiliza una función flecha para realizar
 esta operación. */
+
+let obtenerNumerosPares = (numeros) => {
+    return numeros.filter(numero => numero % 2 === 0);
+};
+
+// Ejemplo de uso:
+const arrayOriginal = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const arrayPares = obtenerNumerosPares(arrayOriginal);
+
+document.write(arrayPares);
--- Cliente/Actividad 3.0/009getFactorial.js
+++ Cliente/Actividad 3.0/009getFactorial.js
@@ -3,3 +3,18 @@
 /*Escribe una función que tome un número entero positivo como argumento y devuelva su factorial.
  El factorial de un número es el producto de todos los enteros positivos desde 1 hasta ese número.
 */
+
+function calcularFactorial(numero) {
+    if (numero < 0) {
+        return "Pon un numero positivo";
+    } else if (numero === 0 || numero === 1) {
+        return 1;
+    } else {
+        let factorial = 1;
+        for (let i = 2; i <= numero; i++) {
+            factorial *= i;
+        }
+        return factorial;
+    }
+}
+alert(calcularFactorial(6));

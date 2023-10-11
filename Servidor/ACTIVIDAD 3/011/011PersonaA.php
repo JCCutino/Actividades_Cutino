daed5bdb23535bb2bc3b4578459625e00bfffe04
JCCutino <jcutinocortacero@alumnos.ilernasevilla.com>
2023-10-11 18:48:59 +0200

    Actividad 11

--- Servidor/ACTIVIDAD 3/011/011PersonaA.php
+++ Servidor/ACTIVIDAD 3/011/011PersonaA.php
@@ -3,29 +3,30 @@
 Transforma Persona a una clase abstracta donde su método estático toHtml(Persona $p) tenga que ser redefinido en todos sus hijos.
  
 */
-public function __construct(
-    protected string $nombre, 
-    protected string $apellidos,
-    protected int $edad
+abstract class Persona {
+    public function __construct(
+        protected string $nombre, 
+        protected string $apellidos,
+        protected int $edad
     ) {}
 
-public function getNombreCompleto(): string {
-    return $this->nombre . " " . $this->apellidos;
+    abstract public function toHtml(): string;
+
+    public function getNombreCompleto(): string {
+        return $this->nombre . " " . $this->apellidos;
+    }
+
+    public function getNombre(): string {
+        return $this->nombre;
+    }
+
+    public function getApellidos(): string {
+        return $this->apellidos;
+    }
+
+    public function __toString(): string {
+        return $this->toHtml();
+    }
 }
 
-public function getNombre(): string {
-    return $this->nombre;
-}
-
-public function getApellidos(): string {
-    return $this->apellidos;
-}
-
-public function __toString(): string {
-    $info = "<p>";
-    $info .= "<strong>Nombre completo:</strong> " . $this->getNombreCompleto() . "<br>";
-    $info .= "<strong>Edad:</strong> " . $this->edad . "<br>";
-    $info .= "</p>";
-    return $info;
-}
 ?>

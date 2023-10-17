32d34bbd01826801f02e23831a223d8397e5e7c3
Juan Cutino <jcutinocortacero@alumos.ilernasevilla.com>
2023-10-17 12:58:33 +0200

    Use strict

---
+++ Cliente/Actividad 4.0/001user.js
@@ -0,0 +1,8 @@
+"use strict";
+/*Escribe el código, una línea para cada acción:
+Crea un objeto user vacío.
+Agrega la propiedad name con el valor John Snow.
+Agrega la propiedad surname con el valor Sam Sagaz.
+Cambia el valor de name a Peter Pan.
+Quita la propiedad name del objeto.
+ */

ad32ad0ddb7bff9e8919636fc5c9214aae389783
JCCutino <jcutinocortacero@alumnos.ilernasevilla.com>
2023-10-24 19:28:57 +0200

    Actividad 17

--- Cliente/Actividad 4.0/017createRandom.js
+++ Cliente/Actividad 4.0/017createRandom.js
@@ -4,3 +4,17 @@ alert( random(1, 5) ); // 1.2345623452
 alert( random(1, 5) ); // 3.7894332423
 alert( random(1, 5) ); // 4.3435234525
  */
+
+function random(min, max) {
+    // Gerenamos un valor aleatorio entre 0 y 1(no inclusive)
+   let valorRandom = Math.random();
+  
+   //Para generar el resultado sumamos el minimo al producto del valor aleatorio por el maximo menos el minimo
+   let resultado = min + (valorRandom * (max - min));
+  
+    return resultado;
+  }
+
+alert(random(1, 5) ); 
+alert(random(1, 5) ); 
+alert(random(1, 5) ); 

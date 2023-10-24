45ad1d921d4ec4a94970540e00bc290ddc8600e2
JCCutino <jcutinocortacero@alumnos.ilernasevilla.com>
2023-10-24 23:29:43 +0200

    Actividad 35

--- Cliente/Actividad 4.0/035getSecondsToAfterTomorrow.js
+++ Cliente/Actividad 4.0/035getSecondsToAfterTomorrow.js
@@ -6,3 +6,21 @@ Por ejemplo, si ahora son las 23:00, entonces:
 getSecondsToAfterTomorrow() == 3600 + todos los segundo de un día
 P.D.: La función debe poder funcionar para cualquier día, sin valores fijos en el código”.
 */
+
+  function getSecondsToAfterTomorrow() {
+    
+    let ahora = new Date();//Obtenemos fecha actual
+  
+    //Obtenemos la fecha de mañana(actual+1)
+    let mañana = new Date(ahora.getFullYear(), ahora.getMonth(), ahora.getDate()+1);
+
+ //Obtenemos la fecha de pasado mañana(mañana+1)
+    let pasadoMañana = new Date(mañana.getFullYear(), mañana.getMonth(), mañana.getDate()+1);
+  
+    let diferencia = pasadoMañana - ahora; //Calculamos la diferencia en milisegundos
+    return Math.round(diferencia / 1000); //Redondeamos
+  }
+
+  const secondsToAfterTomorrow = getSecondsToAfterTomorrow(); //Llamamos a la funcion 
+
+alert(secondsToAfterTomorrow);

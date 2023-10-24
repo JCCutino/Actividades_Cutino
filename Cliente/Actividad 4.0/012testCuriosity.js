668c711cbb6563c776f3d164411f87768b4ca182
Juan Cutino <jcutinocortacero@alumos.ilernasevilla.com>
2023-10-24 12:50:35 +0200

    Actividad 12

--- Cliente/Actividad 4.0/012testCuriosity.js
+++ Cliente/Actividad 4.0/012testCuriosity.js
@@ -3,3 +3,8 @@
 alert( 9999999999999999 );
 ¿Qué está pasando?¿Por qué?
  */
+
+alert( 9999999999999999 );
+
+//Al introducir un valo tan grande JavaScript no lo almacena de forma precisa
+//al mostrarlo se muestra el siguiente número del que se introdujo

571bc815763ac33fb81d4c892a35a1553297285c
JCCutino <jcutinocortacero@alumnos.ilernasevilla.com>
2023-10-24 20:58:49 +0200

    Actividad 25 (falta barajar)

--- Cliente/Actividad 4.0/025usingArrayDisney.js
+++ Cliente/Actividad 4.0/025usingArrayDisney.js
@@ -17,3 +17,40 @@
 //Buscar al cocodrilo.
 //Escribe la función shuffle(array) que baraje (reordene de forma aleatoria) los elementos del array.
  */
+
+let disney = [
+    { nombre: "Hércules", pelicula: "Hércules" },
+    { nombre: "Megana", pelicula: "Hércules" },
+    { nombre: "Hades", pelicula: "Hércules" },
+    { nombre: "Campanilla", pelicula: "Peter Pan" },
+    { nombre: "Wendy", pelicula: "Peter Pan" }
+];
+
+// Meter a "Peter Pan" al final del array disney.
+disney.push({ nombre: "Peter Pan", pelicula: "Peter Pan" });
+
+// Pintar el array
+console.log(disney);
+
+// Meter a "El capitán Garfio" al principio del array
+disney.unshift({ nombre: "El capitán Garfio", pelicula: "Peter Pan" });
+
+// Meter al cocodrilo detrás del capitán
+disney.splice(2, 0, { nombre: "Cocodrilo", pelicula: "Peter Pan" });
+
+// Obtener los personajes de la película "Peter Pan" y mostrarlos uno a uno.
+for (let personaje of disney) {
+    if (personaje.pelicula === "Peter Pan") {
+        console.log(personaje.nombre);
+    }
+}
+
+// Encontrar el índice de "Campanilla".
+const indiceCampanilla = disney.findIndex(personaje => personaje.nombre === "Campanilla");
+console.log("Índice de Campanilla:", indiceCampanilla);
+
+// Buscar al cocodrilo.
+const cocodrilo = disney.find(personaje => personaje.nombre === "Cocodrilo");
+console.log("Cocodrilo:", cocodrilo);
+
+//Escribe la función shuffle(array) que baraje (reordene de forma aleatoria) los elementos del array.y.

712ea4bad987120fb8e5f6e7935adcff31960db2
JCCutino <jcutinocortacero@alumnos.ilernasevilla.com>
2023-10-24 20:37:18 +0200

    Actividad 23

--- Cliente/Actividad 4.0/023modifyThor.js
+++ Cliente/Actividad 4.0/023modifyThor.js
@@ -12,3 +12,30 @@ let thor = "Thor es el hijo de Odín";
 //¿Puedes encontrar la letra a? Demuéstralo
 //Pon la mitad de la cadena en minúsculas (Sin usar las posiciones de 0 a 11)
  */
+
+let thor = "Thor es el hijo de Odín";
+// Largo de la cadena
+alert(thor.length);
+// Encontrar la letra "o"
+alert(thor.indexOf("o"));
+// Encontrar el carácter que ocupa la posición 3
+alert(thor[3]);
+// ¿Qué carácter ocupa la posición 1?
+alert(thor[1]);
+// Trocea la cadena usando los espacios en blanco
+alert(thor.split(" ").join("\n")); 
+// Reemplaza "Thor" por "Loki"
+let loki = thor.replace("Thor", "Loki");
+alert(loki);
+// Recorta la cadena para que devuelva la palabra "Odín"
+alert(thor.slice(thor.indexOf("Odín"), thor.indexOf("Odín") + 4));
+
+// ¿Puedes encontrar la letra "a"? Demuéstralo
+alert(thor.includes("a"));
+//Pon la mitad de la cadena en minúsculas (Sin usar las posiciones de 0 a 11)
+
+let primeraMitad = thor.slice(0, thor.length / 2);
+let segundaMitad = thor.slice(thor.length / 2);
+segundaMitad = segundaMitad.toLowerCase();
+thor = primeraMitad + segundaMitad;
+alert(thor)

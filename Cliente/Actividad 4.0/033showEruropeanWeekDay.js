2ff7183fc5ea06e8289450984e41e1fb0121d4d5
JCCutino <jcutinocortacero@alumnos.ilernasevilla.com>
2023-10-24 22:54:49 +0200

    Actividad 33, 34

--- Cliente/Actividad 4.0/033showEruropeanWeekDay.js
+++ Cliente/Actividad 4.0/033showEruropeanWeekDay.js
@@ -3,3 +3,28 @@
 alert( getLocalDay(date) ); // tuesday, should show 2
 Una vez lo obtengas, tradúcelo a español.
  */
+
+function getLocalDay(date) {
+    // Obtenemos el número del día de la semana
+    let diaSemana = date.getDay();
+  
+    // Ajustamos el número para que el domingo sea 7 y el lunes sea el primero
+    if (diaSemana === 0) {
+        diaSemana = 7; 
+    }
+  
+    return diaSemana;
+  }
+
+  function traducirDiaSemanaEspanyol(numeroDia) {
+  let diasSemana = ['Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado', 'Domingo']; //Creamos un array con los dias de la semana
+  return diasSemana[numeroDia - 1]; //Devolvemos el valor del array
+}
+
+let date = new Date(2012, 0, 3);//Creamos un objeto Date
+//Llamamos a los metodos
+let diaSemanaEuropeo = getLocalDay(date);
+alert( diaSemanaEuropeo );
+let diaSemanaEspanyol = traducirDiaSemanaEspanyol(diaSemanaEuropeo);
+
+alert(diaSemanaEspanyol);
--- Cliente/Actividad 4.0/034findLastDayOfMonth.js
+++ Cliente/Actividad 4.0/034findLastDayOfMonth.js
@@ -4,3 +4,19 @@ year – el año en formato de cuatro dígitos, por ejemplo 2012.
 month – el mes, de 0 a 11.
 Por ejemplo, getLastDayOfMonth(2012, 1) = 29 (febrero, año bisiesto).
  */
+
+function getLastDayOfMonth(year, month) {
+    // Creamos un objeto Date para el primer día del siguiente mes y luego restamos un día
+    let siguienteMes = new Date(year, month + 1, 1);
+    let diaAnterior = new Date(siguienteMes - 1);
+  
+    // Obtenemos el día del mes
+    return diaAnterior.getDate();
+  }
+  
+  // Ejemplo de uso
+  let year = 2012;
+  let month = 1; 
+  let ultimoDia = getLastDayOfMonth(year, month);
+  
+  console.log(ultimoDia);

a4c6e78ed678aac8c855eb77da7c8e99d47dcd5b
JCCutino <jcutinocortacero@alumnos.ilernasevilla.com>
2023-10-24 20:16:31 +0200

    Actividad 21

--- Cliente/Actividad 4.0/021truncateText.js
+++ Cliente/Actividad 4.0/021truncateText.js
@@ -4,3 +4,18 @@
 truncate("Lo que me gustaría contar sobre este tema es:", 20) = "Lo que me gustaría c…"
 truncate("Hola a todos!", 20) = "Hola a todos!"
  */
+
+function truncate(str, maxlength) {
+
+if (str.length >= maxlength) { //Comprobamos la longitud de la cadena
+    let cadena = str.slice(0,maxlength)+"..."; //Si es mayor que la longitud maxima hacemos un slice y añadimos ...
+    return cadena;
+} else {
+    return str; //Si no devolvemos la cadena intacta
+}
+
+}
+
+
+alert(truncate("Lo que me gustaría contar sobre este tema es:", 20));
+alert(truncate("Hola a todos!", 20));

52b8eebd339dbe3ce0ed6572b27175068a902375
JCCutino <jcutinocortacero@alumnos.ilernasevilla.com>
2023-10-24 19:08:01 +0200

    Actividad 16

--- Cliente/Actividad 4.0/016fixInfinityLoop.js
+++ Cliente/Actividad 4.0/016fixInfinityLoop.js
@@ -5,3 +5,6 @@ while (i != 10) {
   i += 0.2;
 }
  */
+
+//Esto es debido a los numeros en punto flotante, estos no se representan con presecion en js, por lo cual el valor nunca será 10,
+//si no que se acercará muchisimo (9,999999999), esto resulta en un bucle infinito

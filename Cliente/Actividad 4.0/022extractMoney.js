963b514161560dad3b680ff7c5bb041102ebc8fc
JCCutino <jcutinocortacero@alumnos.ilernasevilla.com>
2023-10-24 20:21:40 +0200

    Actividad 22

--- Cliente/Actividad 4.0/022extractMoney.js
+++ Cliente/Actividad 4.0/022extractMoney.js
@@ -2,3 +2,10 @@
 /*022extractMoney.js/.html: Tenemos un coste en forma de “$120”. Es decir: el signo de dólar va primero y luego el número. Crea una función extractCurrencyValue(str) que extraiga el valor numérico de dicho string y lo devuelva. Por ejemplo:
 alert( extractCurrencyValue('$120') === 120 ); // true
  */
+
+function extractCurrencyValue(str) {
+    // Usamos el slice para eliminar el primer valor ($) y luego usamos un parseInt para transformarlo en un número entero
+    return parseInt(str.slice(1));
+}
+
+alert(extractCurrencyValue('$120')); 

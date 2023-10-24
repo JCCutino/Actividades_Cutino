f65f933da0f92e2d016c7ff8d03a4949e23ea2ce
Juan Cutino <jcutinocortacero@alumos.ilernasevilla.com>
2023-10-24 13:16:04 +0200

    Actividad 13, 14

--- Cliente/Actividad 4.0/013sumVisitorNumbers.js
+++ Cliente/Actividad 4.0/013sumVisitorNumbers.js
@@ -1,3 +1,10 @@
 "use strict";
 /*013sumVisitorNumbers.js/.html: Crea un script que pida al visitante que ingrese dos números y muestre su suma.
 P.D. Hay una trampa con los tipos de valores. */
+
+let a = +prompt("Introduce un número: "); //Pedimos un valor y lo convertimos a un valor numerico
+
+let b = +prompt("Introduce otro número: "); //Hacemos lo mismo
+
+let resultado = a+b; //Lo sumamos y mostramos el resultado
+alert(resultado);
--- Cliente/Actividad 4.0/014fixNumbers.js
+++ Cliente/Actividad 4.0/014fixNumbers.js
@@ -4,3 +4,14 @@ En el ejemplo similar que sigue, ¿por qué 6.35 es redondeado a 6.3, y no a 6.4
 alert( 6.35.toFixed(1) ); // 6.3
 ¿Cómo redondear 6.35 de manera correcta?
  */
+
+
+alert( 6.35.toFixed(1) );
+
+//Esto se deba a la forma en la que redondea JS, al ser el segundo valor redondea para abajo
+
+//Para hacerlo podemos multiplicar al numero por 10, redondearlo con la funcion Math.round y luego dividirlo entre 10
+
+var num = 6.35;
+var numRedondeado = Math.round(num * 10) / 10;
+alert(numRedondeado); // Esto mostrará 6.4

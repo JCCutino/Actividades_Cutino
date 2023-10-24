db3dc22234399fb38811fd184a38e51c385a11e0
Juan Cutino <jcutinocortacero@alumos.ilernasevilla.com>
2023-10-24 12:41:07 +0200

    Actividad 11

--- Cliente/Actividad 4.0/011addPropertyToString.js
+++ Cliente/Actividad 4.0/011addPropertyToString.js
@@ -5,3 +5,9 @@ str.test = 5;
 alert(str.test);
 Qué piensas: ¿funcionará? ¿Qué mostrará? ¿Por qué?
  */
+
+let str = "Peter";
+str.test = 5;
+alert(str.test);
+
+//La variable str es una variable de tipo primitivo(String), con lo cual no podemos asignarle una propiedad

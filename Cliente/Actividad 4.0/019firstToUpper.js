189662d4f6454ec0663e4ebdc41ca540ced012f6
JCCutino <jcutinocortacero@alumnos.ilernasevilla.com>
2023-10-24 19:44:20 +0200

    Actividad 19

--- Cliente/Actividad 4.0/019firstToUpper.js
+++ Cliente/Actividad 4.0/019firstToUpper.js
@@ -2,3 +2,10 @@
 /*019firstToUpper.js/.html: Escribe una función ucFirst(str) que devuelva el string str con el primer carácter en mayúscula, por ejemplo:
 ucFirst("john") == "John";
  */
+
+function ucFirst(str) {
+      // Convierte el primer carácter a mayúscula y concatena el resto de la cadena
+      return str[0].toUpperCase() + str.slice(1);
+    }
+  
+    alert(ucFirst("holaaa"));

3059a1cc9bb0d3494f09790bcd090613cc662a1c
JCCutino <jcutinocortacero@alumnos.ilernasevilla.com>
2023-10-24 21:35:22 +0200

    Actividad 28

--- Cliente/Actividad 4.0/028mapToName.js
+++ Cliente/Actividad 4.0/028mapToName.js
@@ -8,3 +8,15 @@ let bands = [ acdc, metallica, guns];
 let names = /* ... tu código 
 alert( names );
 */
+
+let acdc= { name: "ACDC", age: 25 };
+let metallica = { name: "Metallica", age: 30 };
+let guns= { name: "Guns and Roses", age: 28 };
+let bands = [ acdc, metallica, guns];
+
+//Con el metodo map vamos  iterando en los objetos y almacenamos el valor name en el array names
+let names = bands.map(band => band.name);
+
+let namesString = names.join(', ');//Lo convertimos a un string usando el metodo join diviendolos con comas
+
+alert(namesString);

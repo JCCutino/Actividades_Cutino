3714ff48109158f90b9d78bd4d4278ea4b0a0654
JCCutino <122985944+JCCutino@users.noreply.github.com>
2023-10-25 09:52:49 +0200

    Actualizar 029practiceWithBands.js

--- Cliente/Actividad 4.0/029practiceWithBands.js
+++ Cliente/Actividad 4.0/029practiceWithBands.js
@@ -6,7 +6,7 @@
     {nombre: "NCT Dream", genero: "K-Pop"},
     {nombre: "Metallica", genero: "Heavy Metal"}
 ];
-
+Hola Samu
 //filtra los grupos por género obtenido aquellos que son de Heavy Metal.
 //Busca en los grupos usando “find” el grupo con Nombre “Cold Play”.
 //¿En qué posición del array se encuenta “Cold Play”?

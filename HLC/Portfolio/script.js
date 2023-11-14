4c4cff303f25ef1b4d9380180ee1f39ebc0a75c6
JCCutino <jcutinocortacero@alumnos.ilernasevilla.com>
2023-11-14 19:05:07 +0100

    Terminal de datos finalizada (completar datos(

--- HLC/Portfolio/script.js
+++
@@ -1,7 +0,0 @@
-class Juan_Cutiño {
-            constructor(){ 
-              this.nombre = 'Juan Cutiño' 
-              this.email = 'jcutinocortacero@gmail.com'
-              this.numTelefono = 653652958 
-            }
-        }

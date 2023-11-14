ba1e3eeef139e92ad4eb9df4e93fcc8619b01252
JCCutino <jcutinocortacero@alumnos.ilernasevilla.com>
2023-11-14 16:22:40 +0100

    Navbar e inicio de consola de datos

---
+++ HLC/Portfolio/script.js
@@ -0,0 +1,7 @@
+class Juan_Cutiño {
+            constructor(){ 
+              this.nombre = 'Juan Cutiño' 
+              this.email = 'jcutinocortacero@gmail.com'
+              this.numTelefono = 653652958 
+            }
+        }

e05151b6e2f048a9085695b67f0fb095a7bc12db
Juan Cutino <jcutinocortacero@alumos.ilernasevilla.com>
2023-11-17 09:49:04 +0100

    script atropos, fila nueva, overflow terminal

---
+++ HLC/Portfolio/atropos/atropos.js
@@ -0,0 +1,44 @@
+const atropos1 = Atropos({
+    el: '.atropos1',
+    activeOffset: 40,
+    shadow: false,
+    highlight: true
+
+  });
+
+  const atropos2 = new Atropos({
+    el: '.atropos2',
+    shadow: false
+  });
+
+  const atropos3 = new Atropos({
+    el: '.atropos3',
+    shadow: false
+  });
+
+  const atropos4 = new Atropos({
+    el: '.atropos4',
+    shadow: false
+  });
+
+  const atropos5 = Atropos({
+    el: '.atropos5',
+    activeOffset: 40,
+    shadow: false,
+
+  });
+
+  const atropos6 = new Atropos({
+    el: '.atropos6',
+    shadow: false
+  });
+
+  const atropos7 = new Atropos({
+    el: '.atropos7',
+    shadow: false
+  });
+
+  const atropos8 = new Atropos({
+    el: '.atropos8',
+    shadow: false
+  });

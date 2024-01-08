a04f0846bdb382b5f609db6c16774dfdc1e35e5d
JCCutino <jcutinocortacero@alumnos.ilernasevilla.com>
2024-01-08 14:12:27 +0100

    Cambios animación

--- HLC/CuestaEnero/js/script.js
+++ HLC/CuestaEnero/js/script.js
@@ -1,4 +1,35 @@
-function toggleContent(sectionId) {
-    var content = document.getElementById(sectionId);
-    content.classList.toggle('hidden');
-  }
+document.addEventListener("DOMContentLoaded", function () {
+    window.addEventListener("scroll", revealOnScrollLeft);
+    window.addEventListener("scroll", revealOnScrollRight);
+
+    function revealOnScrollLeft() {
+        var elements = document.querySelectorAll(".scroll-animation-left");
+
+        for (var i = 0; i < elements.length; i++) {
+            var windowHeight = window.innerHeight;
+            var elementTop = elements[i].getBoundingClientRect().top;
+            var distance = windowHeight - elementTop;
+
+            if (distance > 0) {
+                elements[i].classList.add("active");
+            }
+        }
+    }
+
+    function revealOnScrollRight() {
+        var elements = document.querySelectorAll(".scroll-animation-right");
+
+        for (var i = 0; i < elements.length; i++) {
+            var windowHeight = window.innerHeight;
+            var elementTop = elements[i].getBoundingClientRect().top;
+            var distance = windowHeight - elementTop;
+
+            if (distance > 0) {
+                elements[i].classList.add("active");
+            }
+        }
+    }
+
+    revealOnScrollLeft();
+    revealOnScrollRight();
+});

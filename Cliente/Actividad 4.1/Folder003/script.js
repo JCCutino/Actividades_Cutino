760f17a31734c631e6cdbbaa5d85e0fccb7e6ccd
JCCutino <jcutinocortacero@alumnos.ilernasevilla.com>
2024-01-09 14:24:35 +0100

    Preparacion 3 y 4

---
+++ Cliente/Actividad 4.1/Folder003/script.js
@@ -0,0 +1,26 @@
+//001 - Usando JavaScript, selecciona el enlace (<a>) y verifica si tiene el atributo target. 
+//Si lo tiene, imprime en la consola su valor, de lo contrario, imprime "El atributo target no está presente".
+
+
+
+//002 - Usando JavaScript, selecciona el primer elemento li dentro de la lista y verifica si tiene el atributo data-tipo.
+// Si lo tiene, imprime en la consola su valor, de lo contrario, imprime "El atributo data-tipo no está presente".
+
+
+
+//003 - Usando JavaScript, selecciona el párrafo con clase parrafo y cambia su atributo 
+//data-tipo a "nuevo-parrafo". Luego, imprime en la consola el nuevo valor del atributo.
+
+
+
+//004 - Usando JavaScript, selecciona el título (<h1>) y agrega un nuevo atributo llamado style con el valor color: blue;.
+
+
+
+//005 - Usando JavaScript, selecciona el primer elemento li y elimina su atributo data-tipo. Luego, imprime en la consola la lista de atributos restantes.
+
+
+
+//006 - Usando JavaScript, selecciona el enlace (<a>) y verifica si tiene algún atributo personalizado 
+//en el dataset. Si los tiene, imprime en la consola todos los atributos personalizados presentes.
+

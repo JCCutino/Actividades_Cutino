6055e9516fdb47f0e02819d9a3e8b23e3533ab31
JCCutino <jcutinocortacero@alumnos.ilernasevilla.com>
2024-01-14 16:59:51 +0100

    Actividad 009

---
+++ Cliente/Actividad 4.2/009hideMessageAfterClose/009hideMessageAfterClose.js
@@ -0,0 +1,23 @@
+/*
+009hideMessageAfterClose.html/.js: Dale funcionalidad al botón de cierre que has
+creado para la lista de mensajes del ejercicio anterior. Es decir, haz que funcionen.
+ 
+ */
+//He leido este ejercicio despues que el otro, así que el codigo es exactamente igual porque ya habia implementado el cierre 
+
+let listaMensajes = document.getElementById('listaMensajes');
+
+// Agrega un botón de cierre a cada mensaje
+let mensajes = listaMensajes.querySelectorAll('.mensaje');
+mensajes.forEach((mensaje) => {
+  const botonCierre = document.createElement('span');
+  botonCierre.classList.add('boton-cierre');
+  botonCierre.innerHTML = '[X]'; 
+  botonCierre.addEventListener('click', function () {
+   
+    mensaje.remove();
+  });
+
+  // Agrega el botón de cierre al mensaje
+  mensaje.appendChild(botonCierre);
+});
